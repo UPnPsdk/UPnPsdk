4b246bc503332163df470840e988a6ed72556909
Ingo <ingo@hoeft-online.de>
2024-10-24 19:07:20 +0200

    test(SSDP): Reactivate and rework ssdp Unit tests

--- Utest/compa/1-discovery/test_ssdp.cpp
+++ Utest/compa/1-discovery/test_ssdp.cpp
@@ -239,11 +239,10 @@ TEST_F(SsdpMockFTestSuite, get_ssdp_sockets) {
 
     // Manage IPv6 ssdp socket 'sock6_bind'
     // ------------------------------------
-#if !defined(__APPLE__)
     EXPECT_CALL(m_sys_socketObj,
                 setsockopt(sock6_bind, SOL_SOCKET, SO_REUSEADDR, _, _))
+        // setsockopt(1004, 65535, 4, 0x7ff7b103bebc, 4)
         .WillOnce(Return(0));
-#endif
 #if (defined(BSD) && !defined(__GNU__)) || defined(__APPLE__)
     EXPECT_CALL(m_sys_socketObj,
                 setsockopt(sock6_bind, SOL_SOCKET, SO_REUSEPORT, _, _))
@@ -266,8 +265,9 @@ TEST_F(SsdpMockFTestSuite, get_ssdp_sockets) {
 
     // Provide needed data.
     MiniServerSockArray mini_sock{};
-    std::strcpy(gIF_IPV6, "2001:db8::55");
+    // std::strcpy(gIF_IPV6, "2001:db8::55");
     // std::strcpy(gIF_IPV6_ULA_GUA, "fe80::55");
+    std::strcpy(gIF_IPV6_ULA_GUA, "2001:db8::55");
 
     // Test Unit
     int ret_get_ssdp_socket = get_ssdp_sockets(&mini_sock);
