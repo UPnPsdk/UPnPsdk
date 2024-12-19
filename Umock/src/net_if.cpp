1a4e1bd2002487730b5edd3086644f1659fca0e9
Ingo <ingo@hoeft-online.de>
2024-12-19 14:45:58 +0100

    re-en(addressing): improve global winsock init

--- Umock/src/net_if.cpp
+++ Umock/src/net_if.cpp
@@ -1,7 +1,8 @@
 // Copyright (C) 2022 GPL 3 and higher by Ingo Höft,  <Ingo@Hoeft-online.de>
-// Redistribution only with this Copyright remark. Last modified: 2024-12-19
+// Redistribution only with this Copyright remark. Last modified: 2024-08-17
 
 #include <umock/net_if.hpp>
+#include <UPnPsdk/port.hpp>
 
 namespace umock {
 
