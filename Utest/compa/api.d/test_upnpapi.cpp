f4304c7f2ba422044a5da38af4152ff91c28b862
Ingo <ingo@hoeft-online.de>
2025-03-13 09:36:14 +0100

    re-en(addressing): improve UpnpInit2() arguments

--- Utest/compa/api.d/test_upnpapi.cpp
+++ Utest/compa/api.d/test_upnpapi.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2021+ GPL 3 and higher by Ingo Höft, <Ingo@Hoeft-online.de>
-// Redistribution only with this Copyright remark. Last modified: 2025-03-12
+// Redistribution only with this Copyright remark. Last modified: 2025-03-13
 
 #ifdef UPnPsdk_WITH_NATIVE_PUPNP
 #include <Pupnp/upnp/src/api/upnpapi.cpp>
@@ -846,6 +846,8 @@ TEST_F(UpnpapiFTestSuite, UpnpGetIfInfo_with_ifname_successful) {
     nadaptObj.find_first(index);
     do {
         nadaptObj.sockaddr(saObj);
+        std::cerr << "DEBUG! current selected ip address=\"" << saObj
+                  << "\".\n";
         if (saObj.ss.ss_family == AF_INET6 &&
             (IN6_IS_ADDR_LINKLOCAL(&saObj.sin6.sin6_addr) ||
              IN6_IS_ADDR_LOOPBACK(&saObj.sin6.sin6_addr)))
@@ -871,6 +873,8 @@ TEST_F(UpnpapiFTestSuite, UpnpGetIfInfo_with_ifname_successful) {
 
     EXPECT_STREQ(gIF_NAME, adapt_name.c_str());
     EXPECT_EQ(gIF_INDEX, index);
+    EXPECT_EQ(gIF_LOOPBACK[0], '\0');
+    EXPECT_EQ(gIF_LOOPBACK_PREFIX_LENGTH, 0);
     if (found_lla) {
         EXPECT_NE(gIF_IPV6[0], '\0');
         EXPECT_NE(gIF_IPV6_PREFIX_LENGTH, 0);
