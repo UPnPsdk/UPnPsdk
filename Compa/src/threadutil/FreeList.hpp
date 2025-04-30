831f709066a8416f70246a464d49552da134b370
Ingo <ingo@hoeft-online.de>
2025-04-30 17:57:24 +0200

    re-en(operating): create pthread scoped mutex lock

--- Compa/src/threadutil/FreeList.hpp
+++ Compa/src/threadutil/FreeList.hpp
@@ -5,7 +5,7 @@
  * Copyright (c) 2000-2003 Intel Corporation
  * All rights reserved.
  * Copyright (C) 2021+ GPL 3 and higher by Ingo Höft, <Ingo@Hoeft-online.de>
- * Redistribution only with this Copyright remark. Last modified: 2024-03-06
+ * Redistribution only with this Copyright remark. Last modified: 2025-04-30
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -41,8 +41,7 @@
  * The caller must ensure valid parameters.
  */
 
-#include <ithread.hpp>
-#include <UPnPsdk/visibility.hpp>
+#include <UPnPsdk/pthread.hpp>
 /// \cond
 #include <cerrno>
 /// \endcond
--- UPnPsdk/include/UPnPsdk/pthread.hpp
+++ UPnPsdk/include/UPnPsdk/pthread.hpp
@@ -1,42 +1,14 @@
-#ifndef COMPA_ITHREAD_HPP
-#define COMPA_ITHREAD_HPP
-/*******************************************************************************
- *
- * Copyright (c) 2000-2003 Intel Corporation
- * All rights reserved.
- * Copyright (c) 2012 France Telecom All rights reserved.
- * Copyright (C) 2022+ GPL 3 and higher by Ingo Höft, <Ingo@Hoeft-online.de>
- * Redistribution only with this Copyright remark. Last modified: 2025-04-30
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- * * Neither name of Intel Corporation nor the names of its contributors
- * may be used to endorse or promote products derived from this software
- * without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL INTEL OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- ******************************************************************************/
+#ifndef UPnPsdk_PTHREAD_HPP
+#define UPnPsdk_PTHREAD_HPP
+// Copyright (C) 2022+ GPL 3 and higher by Ingo Höft, <Ingo@Hoeft-online.de>
+// Redistribution only with this Copyright remark. Last modified: 2025-04-30
+
 /*!
  * \file
- * \brief Deprecated ithread functions redirected to pthread. Internal use Only.
- * \todo Remove ithread and use pthread direct.
+ * \brief Additional functions to manage Posix threads with C++ and for
+ * Microsoft Windows.
+ *
+ * For internal use Only.
  */
 
 #include "pthread.h" // To find pthreads4w don't use <pthread.h>
@@ -126,4 +98,4 @@ inline int cleanup_thread() {
 
 /// \endcond
 
-#endif /* COMPA_ITHREAD_HPP */
+#endif /* UPnPsdk_PTHREAD_HPP */
