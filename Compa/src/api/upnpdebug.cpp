efa716855c91b1311e198279bacbab382210c48f
Ingo <ingo@hoeft-online.de>
2025-05-01 01:33:29 +0200

    re-en(operating): create pthread scoped mutex lock

--- Compa/src/api/upnpdebug.cpp
+++ Compa/src/api/upnpdebug.cpp
@@ -3,7 +3,7 @@
  * Copyright (c) 2000-2003 Intel Corporation
  * All rights reserved.
  * Copyright (C) 2021+ GPL 3 and higher by Ingo Höft, <Ingo@Hoeft-online.de>
- * Redistribution only with this Copyright remark. Last modified: 2025-04-30
+ * Redistribution only with this Copyright remark. Last modified: 2025-05-01
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -45,6 +45,7 @@
 #include <umock/stdio.hpp>
 
 /// \cond
+#include <cstdarg>
 #include <cstring>
 /// \endcond
 
