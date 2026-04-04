1586a03b63a80db07e153d651b7bf62432354a4e
Ingo <ingo@hoeft-online.de>
2026-04-05 00:33:49 +0200

    re-en(addressing): provide UPnPsdk::socket()

--- Compa/src/genlib/net/http/httpreadwrite.cpp
+++ Compa/src/genlib/net/http/httpreadwrite.cpp
@@ -4,7 +4,7 @@
  * All rights reserved.
  * Copyright (c) 2012 France Telecom All rights reserved.
  * Copyright (C) 2022+ GPL 3 and higher by Ingo Höft, <Ingo@Hoeft-online.de>
- * Redistribution only with this Copyright remark. Last modified: 2026-04-04
+ * Redistribution only with this Copyright remark. Last modified: 2026-04-05
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -50,6 +50,7 @@
 
 /// \cond
 #include <cassert>
+#include <cstdarg> // needed for MacOS
 #ifdef _WIN32
 #include <malloc.h>
 #define fseeko fseek
